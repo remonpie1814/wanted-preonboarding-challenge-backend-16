fc88abcbcaf998ce35eedba3f69bbe19278d875c
YEJIHAN <yeji1814@gmail.com>
2024-01-23 13:36:32 +0900

    emmiter 추가

---
+++ src/main/java/com/wanted/preonboarding/core/emmiter/SseEmitters.java
@@ -0,0 +1,45 @@
+package com.wanted.preonboarding.core.emmiter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@Slf4j
+public class SseEmitters {
+    private static final AtomicLong counter = new AtomicLong();
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+    public SseEmitter add(SseEmitter emitter) {
+        this.emitters.add(emitter);
+        log.info("new emitter added: {}", emitter);
+        log.info("emitter list size: {}", emitters.size());
+        emitter.onCompletion(() -> {
+            log.info("onCompletion callback");
+            this.emitters.remove(emitter);    // 만료되면 리스트에서 삭제
+        });
+        emitter.onTimeout(() -> {
+            log.info("onTimeout callback");
+            emitter.complete();
+        });
+
+        return emitter;
+    }
+
+    public void count() {
+        long count = counter.incrementAndGet();
+        emitters.forEach(emitter -> {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("count")
+                        .data(count));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
